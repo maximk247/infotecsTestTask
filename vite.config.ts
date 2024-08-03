9ca1879f16afe628d407fef33c6d8c6011a3807f
maximk247 <kiyaniytsamaxim@gmail.com>
2024-08-03 19:18:10 +0700

    Исправление конфигурации

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/test-task/',
+  base: '/infotecsTestTask/',
   plugins: [react()],
   test: {
     globals: true,
